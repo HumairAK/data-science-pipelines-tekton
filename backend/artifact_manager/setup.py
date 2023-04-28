29136afc4a49198db1a5460b8ea1e1399590eef5
Humair Khan <humair88@hotmail.com>
2023-04-28 14:39:07 -0400

    Remove downstream mentions.
    Signed-off-by: Humair Khan <humair88@hotmail.com>

--- backend/artifact_manager/setup.py
+++ backend/artifact_manager/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='modh/data-science-pipelines-artifact-manager',
+    name='odh/data-science-pipelines-artifact-manager',
     version='1.0.0',
-    url='https://github.com/red-hat-data-services/data-science-pipelines.git',
-    author='Red Hat Openshift Data Science',
-    author_email='managed-open-data-hub@redhat.com',
-    description='Artifact Manager for Data Science Pipelines on RHODS',
+    url='https://github.com/opendatahub-io/data-science-pipelines.git',
+    author='Open Data Hub',
+    author_email='users@lists.opendatahub.io',
+    description='Artifact Manager for Data Science Pipelines',
     packages=find_packages(),
     install_requires=['awscli >= 1.25.78']
 )
